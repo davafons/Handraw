82702ad6577a44ac3eb412ed3d2f98329172bd0e
Dibad <dibadgithub@gmail.com>
2019-01-11 23:45:59 +0000

    BGSub class refactored

--- src/MyBGSubtractorColor.cpp
+++ src/MyBGSubtractorColor.cpp
@@ -1,43 +1,40 @@
 #include "MyBGSubtractorColor.h"
 
 MyBGSubtractorColor::MyBGSubtractorColor() {
-  cv::namedWindow(win_trackbars_);
-  cv::moveWindow(win_trackbars_, 1400, 50);
-  cv::namedWindow("test");
-  cv::namedWindow("bg");
+  cv::namedWindow(w_trackbars_);
+  cv::moveWindow(w_trackbars_, 1400, 50);
+  cv::namedWindow(w_bg_);
+  cv::namedWindow(w_bg_sub_);
 
-  bg_subtractor_ = cv::createBackgroundSubtractorMOG2(500, 5, false);
-  face_subtractor_.load("res/haarcascade_frontalface_alt.xml");
+  bg_sub_ = cv::createBackgroundSubtractorMOG2(500, 5, false);
 
-  cv::createTrackbar("H low:", win_trackbars_, &h_low_, 100);
-  cv::createTrackbar("H up:", win_trackbars_, &h_up_, 100);
-  cv::createTrackbar("L low:", win_trackbars_, &l_low_, 100);
-  cv::createTrackbar("L up:", win_trackbars_, &l_up_, 100);
-  cv::createTrackbar("S low:", win_trackbars_, &s_low_, 100);
-  cv::createTrackbar("S up:", win_trackbars_, &s_up_, 100);
+  cv::createTrackbar("H low:", w_trackbars_, &h_low_, 100);
+  cv::createTrackbar("H up:", w_trackbars_, &h_up_, 100);
+  cv::createTrackbar("L low:", w_trackbars_, &l_low_, 100);
+  cv::createTrackbar("L up:", w_trackbars_, &l_up_, 100);
+  cv::createTrackbar("S low:", w_trackbars_, &s_low_, 100);
+  cv::createTrackbar("S up:", w_trackbars_, &s_up_, 100);
 }
 
 MyBGSubtractorColor::~MyBGSubtractorColor() {
-  cv::destroyWindow(win_trackbars_);
-  cv::destroyWindow("test");
-  cv::destroyWindow("bg");
+  cv::destroyWindow(w_trackbars_);
+  cv::destroyWindow(w_bg_);
+  cv::destroyWindow(w_bg_sub_);
 }
 
 void MyBGSubtractorColor::LearnModel(cv::VideoCapture &cap) {
-  // Create window
-  const std::string window_samples =
+  // Crear ventana
+  const std::string w_samples =
       "Cubre los cuadrados con la mano y pulsa espacio";
-  cv::namedWindow(window_samples);
-  cv::createTrackbar("Sample Size:", window_samples, &sample_size_, 100,
-                     nullptr);
-  cv::createTrackbar("Dist between Samples", window_samples,
-                     &distance_between_samples_, 100, nullptr);
-  cv::createTrackbar("Max horiz samples", window_samples, &max_horiz_samples_,
-                     10, nullptr);
-  cv::createTrackbar("Max vert samples", window_samples, &max_vert_samples_, 10,
-                     nullptr);
 
-  std::vector<cv::Point> samples_positions;
+  cv::namedWindow(w_samples);
+  cv::createTrackbar("Sample Size:", w_samples, &sample_size_, 100);
+  cv::createTrackbar("Dist between Samples", w_samples,
+                     &distance_between_samples_, 100);
+  cv::createTrackbar("Max horiz samples", w_samples, &max_horiz_samples_, 10);
+  cv::createTrackbar("Max vert samples", w_samples, &max_vert_samples_, 10);
+
+  std::vector<cv::Point> sample_rect_positions;
 
   cv::Mat frame;
   while (true) {
@@ -51,8 +48,8 @@ void MyBGSubtractorColor::LearnModel(cv::VideoCapture &cap) {
     cv::Mat tmp_frame;
     frame.copyTo(tmp_frame);
 
-    // Calculate and render sample rect positions
-    samples_positions.clear();
+    // Mostrar los rectángulos para sacar las ROI de la piel
+    sample_rect_positions.clear();
     for (int i = 0; i < max_horiz_samples_; ++i) {
       for (int j = 0; j < max_vert_samples_; ++j) {
         cv::Point p;
@@ -60,21 +57,21 @@ void MyBGSubtractorColor::LearnModel(cv::VideoCapture &cap) {
                                    (sample_size_ + distance_between_samples_);
         p.y = frame.rows / 2 + (-max_vert_samples_ / 2 + j) *
                                    (sample_size_ + distance_between_samples_);
-        samples_positions.push_back(p);
+        sample_rect_positions.push_back(p);
 
-        cv::rectangle(tmp_frame, cv::Rect(p.x, p.y, sample_size_, sample_size_),
-                      cv::Scalar(0, 255, 0), 2);
+        cv::rectangle(tmp_frame, cv::Rect{p.x, p.y, sample_size_, sample_size_},
+                      {0, 255, 0}, 2);
       }
     }
 
     cv::flip(tmp_frame, tmp_frame, 1);
-    cv::imshow(window_samples, tmp_frame);
+    cv::imshow(w_samples, tmp_frame);
 
     if (cv::waitKey(40) == ' ')
       break;
   }
 
-  cv::destroyWindow(window_samples);
+  cv::destroyWindow(w_samples);
 
   // CODIGO 1.1
   // Obtener las regiones de interés y calcular la media de cada una de ellas
@@ -84,9 +81,9 @@ void MyBGSubtractorColor::LearnModel(cv::VideoCapture &cap) {
   cv::cvtColor(frame, hls_frame, cv::COLOR_BGR2HLS);
 
   means_.clear();
-  for (const auto &sample : samples_positions) {
+  for (const auto &sample : sample_rect_positions) {
     cv::Mat roi =
-        hls_frame(cv::Rect(sample.x, sample.y, sample_size_, sample_size_));
+        hls_frame(cv::Rect{sample.x, sample.y, sample_size_, sample_size_});
     means_.push_back(cv::mean(roi));
     std::cout << cv::mean(roi) << std::endl;
   }
@@ -99,13 +96,14 @@ void MyBGSubtractorColor::LearnModel(std::istream &means_file) {
 
     if (means_file.eof())
       break;
+
     means_.push_back(mean);
   }
 
   std::cout << means_.size() << std::endl;
 }
 
-void MyBGSubtractorColor::LearnBGModel(cv::VideoCapture &cap) {
+void MyBGSubtractorColor::LearnBGModel(cv::VideoCapture &cap) const {
   cv::Mat frame;
   cv::Mat temp;
 
@@ -115,28 +113,28 @@ void MyBGSubtractorColor::LearnBGModel(cv::VideoCapture &cap) {
   // Borrar el último fondo guardado
   cv::Mat hls_frame;
   cv::cvtColor(frame, hls_frame, cv::COLOR_BGR2HLS);
-  bg_subtractor_->apply(hls_frame, temp, 1);
+  bg_sub_->apply(hls_frame, temp, 1);
 
-  // Generar un fondo nuevo utilizando tantas imágenes como bg_samples
+  // Generar un fondo nuevo mezclando tantas imágenes como bg_samples
   for (int i = 0; i < bg_samples_; ++i) {
     cap >> frame;
     if (frame.empty())
       continue;
 
     cv::cvtColor(frame, hls_frame, cv::COLOR_BGR2HLS);
-    bg_subtractor_->apply(hls_frame, temp);
+    bg_sub_->apply(hls_frame, temp);
   }
 
-  bg_subtractor_->getBackgroundImage(temp);
-  cv::imshow("bg", temp);
+  bg_sub_->getBackgroundImage(temp);
+  cv::imshow(w_bg_, temp);
 }
 
-void MyBGSubtractorColor::ObtainBGMask(const cv::Mat &frame,
+void MyBGSubtractorColor::ObtainBGMask(const cv::Mat frame,
                                        cv::Mat &bgmask) const {
   cv::Mat hls_frame;
   cv::cvtColor(frame, hls_frame, cv::COLOR_BGR2HLS);
 
-  if (bg_subtractor_enabled_)
+  if (bg_sub_enabled_)
     RemoveBG(hls_frame, hls_frame);
 
   cv::Mat acc = cv::Mat::zeros(frame.size(), CV_8U);
@@ -153,17 +151,14 @@ void MyBGSubtractorColor::ObtainBGMask(const cv::Mat &frame,
     acc += temp_bgmask;
   }
 
-  if (face_subtractor_enabled_)
-    RemoveFace(frame, acc);
-
   acc.copyTo(bgmask);
 }
 
-void MyBGSubtractorColor::RemoveBG(const cv::Mat &frame,
+void MyBGSubtractorColor::RemoveBG(const cv::Mat frame,
                                    cv::Mat &masked_frame) const {
   // Get foreground mask
   cv::Mat foreground_mask;
-  bg_subtractor_->apply(frame, foreground_mask, 0);
+  bg_sub_->apply(frame, foreground_mask, 0);
   cv::medianBlur(foreground_mask, foreground_mask, 3);
 
   // Get a masked frame (without most of the bg)
@@ -171,20 +166,7 @@ void MyBGSubtractorColor::RemoveBG(const cv::Mat &frame,
   cv::bitwise_and(frame, frame, result, foreground_mask);
   masked_frame = result;
 
-  cv::imshow("test", masked_frame);
-}
-
-void MyBGSubtractorColor::RemoveFace(const cv::Mat &frame,
-                                     cv::Mat &bgmask) const {
-  cv::Mat frame_gray;
-  cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
-
-  std::vector<cv::Rect> faces;
-  face_subtractor_.detectMultiScale(frame_gray, faces);
-
-  for (const auto &face : faces) {
-    cv::rectangle(bgmask, face, cv::Scalar(0, 0, 0), cv::FILLED);
-  }
+  cv::imshow(w_bg_sub_, masked_frame);
 }
 
 void MyBGSubtractorColor::clamp(cv::Scalar &s, int low, int up) const {
--- src/MyBGSubtractorColor.h
+++ src/MyBGSubtractorColor.h
@@ -12,49 +12,40 @@ public:
   ~MyBGSubtractorColor();
 
   // Skin Model
-  void LearnModel(cv::VideoCapture &cap);    // From camera
-  void LearnModel(std::istream &means_file); // From file
+  void LearnModel(cv::VideoCapture &cap);    // Desde la camara
+  void LearnModel(std::istream &means_file); // Desde un archivo
 
-  // BG Model
-  void LearnBGModel(cv::VideoCapture &cap);
-  void ObtainBGMask(const cv::Mat &frame, cv::Mat &bgmask) const;
+  // BG Subtraction
+  void LearnBGModel(cv::VideoCapture &cap) const;
+  void ObtainBGMask(const cv::Mat frame, cv::Mat &bgmask) const;
 
-  // Toggle components of the BG subtractor
-  void ToggleBGSubtractor() {
-    bg_subtractor_enabled_ = !bg_subtractor_enabled_;
-  }
-  void ToggleFaceSubtractor() {
-    face_subtractor_enabled_ = !face_subtractor_enabled_;
-  };
+  void ToggleBGSubtractor() { bg_sub_enabled_ = !bg_sub_enabled_; }
 
 private:
-  // Thresholds for skin color detection
+  // Rangos para la detección del color de la piel
   int h_low_{80}, h_up_{7};
   int l_low_{14}, l_up_{80};
   int s_low_{18}, s_up_{70};
   std::vector<cv::Scalar> means_;
 
-  // Settings of samples used in skin model learning
+  // Propiedades de los Rect utilizados para calcular los ROI de la piel
   int max_horiz_samples_{3};
   int max_vert_samples_{6};
   int distance_between_samples_{30};
   int sample_size_{20};
 
-  // Subtractor for removing background before skin detection
-  cv::Ptr<cv::BackgroundSubtractorMOG2> bg_subtractor_;
+  // Objeto encargado de realizar la subtracción de fondo
+  cv::Ptr<cv::BackgroundSubtractorMOG2> bg_sub_;
   int bg_samples_{20};
-  bool bg_subtractor_enabled_{true};
+  bool bg_sub_enabled_{true};
 
-  // Haar cascade classifier for face subtraction (SLOW!)
-  mutable cv::CascadeClassifier face_subtractor_;
-  bool face_subtractor_enabled_;
-
-  // Windows created by this class
-  const std::string win_trackbars_{"Trackbars"};
+  // Windows
+  const std::string w_trackbars_{"Trackbars"};
+  const std::string w_bg_{"Background Model"};
+  const std::string w_bg_sub_{"BG Sub"};
 
 private:
-  void RemoveBG(const cv::Mat &frame, cv::Mat &masked_frame) const;
-  void RemoveFace(const cv::Mat &frame, cv::Mat &bgmask) const;
+  void RemoveBG(const cv::Mat frame, cv::Mat &masked_frame) const;
 
   void clamp(cv::Scalar &s, int low = 0, int up = 255) const;
 };
